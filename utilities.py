a9bcade0d9ebe7c138379a62e8764bb8534a6052
loathingKernel <142770+loathingKernel@users.noreply.github.com>
2025-09-12 20:55:25 +0300

    utilities: don't skip cache cleanup on nvidia

--- utilities.py
+++ utilities.py
@@ -330,7 +330,7 @@ def setup_local_shader_cache(env: dict) -> None:
         '__GL_SHADER_DISK_CACHE_PATH': os.path.join(path, 'nvidiav1'),
         '__GL_SHADER_DISK_CACHE_READ_ONLY_APP_NAME': 'steam_shader_cache;steamapp_merged_shader_cache',
         '__GL_SHADER_DISK_CACHE_SIZE': '10737418240',  # 10GiB
-        '__GL_SHADER_DISK_CACHE_SKIP_CLEANUP': '1',
+        #'__GL_SHADER_DISK_CACHE_SKIP_CLEANUP': '1',
         # Mesa
         'MESA_DISK_CACHE_READ_ONLY_FOZ_DBS': 'steam_cache,steam_precompiled',
         'MESA_DISK_CACHE_SINGLE_FILE': '1',
