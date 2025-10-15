2249b4fc199a7884d5aebff661ef001bc3b0af0f
NelloKudo <marshnelloosu@gmail.com>
2025-10-15 12:09:15 +0200

    patches: allow applying custom patches to Wine
    Allows using patches from the 'spritz' folder.

--- patches/apply.sh
+++ patches/apply.sh
@@ -12,3 +12,13 @@ pushd "$here"/../glslang || exit 1
     patch_cmd "$here"/glslang/glslang-renderdoc-1.36-gcc15-fix.patch
 popd || exit 1
 
+pushd "$here"/../wine || exit 1
+
+if [ -d "$here/spritz" ]; then
+    mapfile -t patches < <(find "$here/spritz" -type f -name "*.patch" | sort)
+    for patch in "${patches[@]}"; do
+        patch_cmd "$patch"
+    done
+fi
+
+popd || exit 1
